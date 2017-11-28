1ca3293fd7a8b73c085e3be36076b2840bd4044c
aaavalenca <32614967+aaavalenca@users.noreply.github.com>
2017-11-28 12:13:14 -0300

    Add files via upload

--- sat_modelo.js
+++ sat_modelo.js
@@ -12,9 +12,105 @@
 }*/
 
 // Receives the current assignment and produces the next one
-function nextAssignment(currentAssignment) {
-  // implement here the code to produce the next assignment based on currentAssignment. 
-  return newAssignment
+
+
+function readFormula(fileName) {
+
+    /*
+  // To read the file, it is possible to use the 'fs' module.
+  // Use function readFileSync and not readFile.
+  // First read the lines of text of the file and only afterward use the auxiliary functions.
+  let text // = ...  //  an array containing lines of text extracted from the file.
+  let clauses = readClauses(text)
+  let variables = readVariables(clauses)
+
+  // In the following line, text is passed as an argument so that the function
+  // is able to extract the problem specification.
+  let specOk = checkProblemSpecification(text, clauses, variables)
+
+  let result = { 'clauses': [], 'variables': [] }
+  if (specOk) {
+    result.clauses = clauses
+    result.variables = variables
+  }
+  return result
+*/
+
+    // Primeiro, vamos pegar o arquivo e ler seu conteúdo para, em seguida, transformá-lo num string utf-8.
+    /* https://stackoverflow.com/questions/34857458/reading-local-text-file-into-a-javascript-array */
+
+    let fs = require("fs"); // http://nodebr.com/como-funciona-a-funcao-require-do-node-js/
+    let textoCompleto = fs.readFileSync(fileName);  /* lê arquivos de forma sincronizada */
+    textoCompleto = textoCompleto.toString('utf-8');  /* transforma o arquivo do buffer no formato uft-8 */
+
+    console.log("O ARQUIVO " + fileName + " CONTÉM:\n");
+    console.log(textoCompleto);
+
+    console.log("\n INÍCIO DE READFORMULA \n");
+
+    let textoCompletoPorLinha = textoCompleto.split("\n");  /* divide o texto conforme as linhas */
+
+    let qtdVariaveis = 0; // armazenará a quantidade de variáveis descritas na linha p
+    let qtdClausulas = 0; // armazenará a quantidade de cláusulas descritas na linha p
+    let clausesContinuedString = ""; // armazenará um string contínuo (concatenado) de todas as cláusulas, consecutivamente.
+
+    // Lê o texto linha-a-linha para ignorar as linhas de comentário (c) e para buscar a quantidade de cláusulas e
+    // variáveis descrita na linha de problema (p) e concatena as cláusulas num só string.
+    for (let i = 0; i < textoCompletoPorLinha.length; i++) {
+        // o slice vai pegar a primeira letra (0,1), para que possamos analisá-la.
+        let primeiraLetra = textoCompletoPorLinha[i].slice(0,1);
+
+        if (primeiraLetra == "c"){ /*ignore c*/
+            //console.log("comentario ignorado");
+
+        } else if(primeiraLetra == "p") {
+            // estou pegando as linhas que começam com "p".
+            // se elas começam com "p", separo-as em elementos (uso o espaço entre elas para isso)
+            // e armazeno-as na variável temp.
+            let temp = textoCompletoPorLinha[i].split(" ");
+            // parseInt transforma o que estava sendo lido como string em um número inteiro.
+            // E aqui, guardamos essas informações nas variáveis.
+            // o número 10 depois de parseInt significa que é no sistema decimal.
+            qtdVariaveis = parseInt(temp[2], 10);
+            qtdClausulas = parseInt(temp[3], 10);
+            // LEMBRAR!!! HÁ A POSSIBILIDADE DE HAVER MAIS ESPACOS ENTRE OS ELEMENTOS DO QUE PREVISTO. ENTAO PRECISAMOS
+            // FAZER UM LOOP PARA VARRER TODOS OS ESPACOS E TIRA-LOS, ASSIM COMO FIZEMOS UM POUCO MAIS ABAIXO, PARA AS CLAUSULAS
+
+        } else {
+            //o que fiz aqui? Concatenei os elementos da equacaoTextual em uma só linha de texto, ou seja, em um só string.
+            //Isto gera uma linha só, textual, com todos os números (inclusive os 0). Vamos depois dar split
+            //para separar em elementos de array.
+            clausesContinuedString = clausesContinuedString.concat(" ", textoCompletoPorLinha[i]);
+        };
+
+
+    };
+
+
+    let specOk = checkProblemSpecification(clausesContinuedString, qtdClausulas, qtdVariaveis);
+    // objetos de retorno do readFormula.
+    let result = { 'clauses': [], 'variables': [] };
+
+    if (specOk) {
+        // Leitura da cláusula.
+        let clauses = readClauses(clausesContinuedString);
+        // Cria um array de falses, com uma quantidade de elementos igual ao número de variáveis.
+        let variables = readVariables(qtdVariaveis);
+
+        // o objeto clauses dentro de results passa a ser igual a clauses
+        result.clauses = clauses;
+        result.variables = variables;
+
+
+    }
+
+
+    console.log("\nRESULTADO DE READFORMULA:\n");
+    console.log(result);
+
+    console.log("\nFINAL DO READFORMULA");
+    return result;
+
 }
 
 function doSolve(clauses, assignment) {
@@ -36,7 +132,7 @@ function doSolve(clauses, assignment) {
 
 function checkProblemSpecification(text, qtdClausulas, qtdVariaveis){
 
-    console.log("\n VERIFICANDO SE A LINHA DE PROBLEMA ESTÁ CORRETA \n");
+    console.log("\n VERIFICANDO SE A LINHA DE PROBLEMA (p) ESTÁ CORRETA \n");
 
     // ClausesSortedDescendant é o string contínuo de todas as cláusulas, com os sinais de menos/not (-) retirados, divididos por um
     // espaco e ordenados de forma decrescente. Assim, o primeiro elemento corresponde ao número mais alto e, consequentemente,
@@ -79,10 +175,9 @@ function checkProblemSpecification(text, qtdClausulas, qtdVariaveis){
 
 };
 
-
 function readClauses (clausulasEmString){
 
-    console.log("\n SEPARANDO AS CLÁUSULAS EM ARRAYS \n");
+    console.log("\nSEPARANDO AS CLÁUSULAS EM ARRAYS \n");
 
     // Cláusulas divididas são o antigo string de cláusulas agora partido a partir de cada elemento 0 (final de linha real).
     let clausesSeparadas = clausulasEmString.split(" 0");
@@ -122,7 +217,7 @@ function readClauses (clausulasEmString){
 
 function readVariables(qtd){
 
-    console.log("\n CRIANDO UM ARRAY DE " + qtd + " falses \n");
+    console.log("\nCRIANDO UM ARRAY DE " + qtd + " falses \n");
 
     // Leitura das variáveis.
     let variablesFalsas = [];
@@ -132,119 +227,39 @@ function readVariables(qtd){
     };
 
     return variablesFalsas;
+
 }
 
-function readFormula(fileName) {
+var fileName = "./hole1.cnf"; // nome do arquivo de entrada
+readFormula(fileName);
 
-    /*
-  // To read the file, it is possible to use the 'fs' module.
-  // Use function readFileSync and not readFile. 
-  // First read the lines of text of the file and only afterward use the auxiliary functions.
-  let text // = ...  //  an array containing lines of text extracted from the file. 
-  let clauses = readClauses(text)
-  let variables = readVariables(clauses)
-  
-  // In the following line, text is passed as an argument so that the function
-  // is able to extract the problem specification.
-  let specOk = checkProblemSpecification(text, clauses, variables)
-
-  let result = { 'clauses': [], 'variables': [] }
-  if (specOk) {
-    result.clauses = clauses
-    result.variables = variables
-  }
-  return result
-*/
-
-    // Primeiro, vamos pegar o arquivo e ler seu conteúdo para, em seguida, transformá-lo num string utf-8.
-    /* https://stackoverflow.com/questions/34857458/reading-local-text-file-into-a-javascript-array */
-
-    let fs = require("fs"); // http://nodebr.com/como-funciona-a-funcao-require-do-node-js/
-    let textoCompleto = fs.readFileSync(fileName);  /* lê arquivos de forma sincronizada */
-    textoCompleto = textoCompleto.toString('utf-8');  /* transforma o arquivo do buffer no formato uft-8 */
-
-    console.log("O ARQUIVO " + fileName + " CONTÉM:\n");
-    console.log(textoCompleto);
-
-    console.log("\n INÍCIO DE READFORMULA \n");
-
-    let textoCompletoPorLinha = textoCompleto.split("\n");  /* divide o texto conforme as linhas */
-
-    let qtdVariaveis = 0; // armazenará a quantidade de variáveis descritas na linha p
-    let qtdClausulas = 0; // armazenará a quantidade de cláusulas descritas na linha p
-    let clausesContinuedString = ""; // armazenará um string contínuo (concatenado) de todas as cláusulas, consecutivamente. É o antigo clausesTemp
-
-    // Lê o texto linha-a-linha para ignorar as linhas de comentário (c) e para buscar a quantidade de cláusulas e
-    // variáveis descrita na linha de problema (p) e concatena as cláusulas num só string.
-    for (let i = 0; i < textoCompletoPorLinha.length; i++) {
-        // o slice vai pegar a primeira letra, para que possamos analisá-la.
-        let primeiraLetra = textoCompletoPorLinha[i].slice(0,1);
-
-        if (primeiraLetra == "c"){ /*ignore c*/
-            //console.log("comentario ignorado");
-
-        } else if(primeiraLetra == "p") {
-            // estou pegando as linhas que começam com "p".
-            // se elas começam com "p", separo-as em elementos (uso o espaço entre elas para isso)
-            // e armazeno-as na variável temp.
-            let temp = textoCompletoPorLinha[i].split(" ");
-            // parseInt transforma o que estava sendo lido como string em um número inteiro.
-            // E aqui, guardamos essas informações nas variáveis.
-            // o número 10 depois de parseInt significa que é no sistema decimal.
-            qtdVariaveis = parseInt(temp[2], 10);
-            qtdClausulas = parseInt(temp[3], 10);
-            // LEMBRAR!!! HÁ A POSSIBILIDADE DE HAVER MAIS ESPACOS ENTRE OS ELEMENTOS DO QUE PREVISTO. ENTAO PRECISAMOS
-            // FAZER UM LOOP PARA VARRER TODOS OS ESPACOS E TIRA-LOS, ASSIM COMO FIZEMOS UM POUCO MAIS ABAIXO, PARA AS CLAUSULAS
-
-        } else {
-            //o que fiz aqui? Concatenei os elementos da equacaoTextual em uma só linha de texto,
-            //ou seja, em uma só string. Essa informação, com a igualdade, foi para jogar a informação lá em cima.
-            //isto gera uma linha só, textual, com todos os números (inclusive o 0. Vamos agora dar split, e talvez slice,
-            //para separar em elementos de array. Lembrar que .push vai jogando elementos numa array (enquanto vão crescendo em função de i).
-            clausesContinuedString = clausesContinuedString.concat(" ", textoCompletoPorLinha[i]);
-        };
+//aqui, peguei o elemento variables do objeto result e transformei numa variável global chamada variablesFinal e vou jogar em nextAssignment
+var variablesFinal = readFormula(fileName).variables;
+//console.log(variablesFinal);
 
 
-    };
+//EM OBRAS
+function nextAssignment(currentAssignment) {
 
+    let newAssignment = [];
 
-    let specOk = checkProblemSpecification(clausesContinuedString, qtdClausulas, qtdVariaveis);
-    // objetos de retorno do readFormula.
-    let result = { 'clauses': [], 'variables': [] };
-    if (specOk) {
-        // Leitura da cláusula.
-        let clauses = readClauses(clausesContinuedString);
-        // Cria um array de falses, com uma quantidade de elementos igual ao número de variáveis.
-        let variables = readVariables(qtdVariaveis);
+    for (let i = 0; i < Math.pow(2, variablesFinal.length); i++){
 
-        // o objeto clauses dentro de results passa a ser igual a clauses
-        result.clauses = clauses;
-        result.variables = variables;
+        newAssignment.push(i.toString(2));
+
+        //console.log(newAssignment);
+
     }
 
-    //console.log(clauses[0][0]); // uma array dentro de outra array. Aqui estou vendo as variáveis dentro das cláusulas
-    console.log("\nO RESULTADO DE READFORMULA:\n");
-    console.log(result);
-//    console.log(clauses);
+    // fazer aqui um loop que faz a varredura, transformando false em verdadeiro quando possível
+    // https://stackoverflow.com/questions/39734887/generating-all-possible-true-false-combinations (?)
 
 
-    console.log("\n FINAL DO READFORMULA");
+    // implement here the code to produce the next assignment based on currentAssignment.
+    return newAssignment;
 
-
-    return result;
 
 }
 
-
-
-/* RESOLVENDO PROBLEMAS SATisfativeis
-  * André Valença
-  * Introducão à Computação
-  * Professor Fernando Castor
-  *
-  * */
-
-
-var fileName = "./tutorial.cnf"; // nome do arquivo de entrada
-
-readFormula(fileName);
+nextAssignment(variablesFinal);
+console.log(nextAssignment(variablesFinal))
